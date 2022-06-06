2a5fdb042d46ce075a33ce411b299f6e45519ea7
EC2 Default User <ec2-user@ip-172-31-43-35.ec2.internal>
2022-06-06 04:16:24 +0000

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import TodoForm from "/react-amplify-app/src/components/TodoForm.js"
+import TodoList from "/react-amplify-app/src/components/TodoList.js"
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  
+  function addTodo(todo){
+    setTodos([todo,...todos]);
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Todo</p>
+        <TodoForm addTodo={addTodo}/>
+        <TodoList todos={todos} />
       </header>
     </div>
   );
 }
 
 export default App;
+
