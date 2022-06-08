9807ebaad7cf811fcff0bc228b9c9d0eb92b3903
melapeyre <mileslapeyre@gmail.com>
2022-06-07 23:17:26 -0500

    Form-commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ function App() {
       < header className = "App-header" >
         <img src={logo} className = "App-logo" alt="logo"/>
         <h1> Hello From A New World 2</h1>
+        <form> 
+        <fieldset>
+         <label>
+           <p>Name</p>
+           <input name="name" />
+         </label>
+       </fieldset>
+       <button type="submit">Submit</button>
+        </form>
       </header>
     </div>
   );
