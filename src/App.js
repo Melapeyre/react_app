fc0544b1d478c8203c111cdf384b6fff68c1aec4
melapeyre <mileslapeyre@gmail.com>
2022-06-10 15:42:56 -0500

    V1 Forms

--- src/App.js
+++ src/App.js
@@ -5,21 +5,47 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     const sku=e.target.SKU.value;
+    const Builder=e.target.Builder.value;
+    const QTY=e.target.QTY.value;
+    const timestamp=e.target.timestamp.value;
+    const Counter=e.target.counter.value;
+    console.log(Builder);
     console.log(sku);
+    console.log(QTY);
+    console.log(timestamp);
+    console.log(counter);
   }
   return (
       <div className = "App">
       < header className = "App-header" >
         <img src={logo} className = "App-logo" alt="logo"/>
         <h1> Hello From A New World 2</h1>
+//production form
         <form onSubmit={handleSubmit}> 
         <fieldset>
          <label>
-           <p>SKU</p>
-           <input type = "text" name="SKU" placeholder = "Enter SKU" />
+           <p>Production Form</p>
+           <input type = "text" name="Builder" placeholder = "Enter Name" />
+           <input type = "text" name="SKU" placeholder = "SKU" />
+           <input type = "number" name ="QTY" placeholder = "QTY Built"/>
+           <input type = "date" name ="timestamp" placeholder = "Date Completed"/>
          </label> 
        </fieldset>
-       <button type="Submit Sku Data">Submit</button>
+       <button type="Submit Production Form">Submit</button>
+        </form>
+//Inventory count form 
+
+<form onSubmit={handleSubmit}> 
+        <fieldset>
+         <label>
+           <p>Production Form</p>
+           <input type = "text" name="Counter" placeholder = "Enter Name" />
+           <input type = "text" name="SKU" placeholder = "SKU" />
+           <input type = "number" name ="QTY" placeholder = "Current Inventory"/>
+           <input type = "date" name ="timestamp" placeholder = "Date Counted"/>
+         </label> 
+       </fieldset>
+       <button type="Submit Inventory Count Form">Submit</button>
         </form>
       </header>
     </div>
