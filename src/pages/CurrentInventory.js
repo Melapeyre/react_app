f52a2fb66642322136fa658e87bc2ef7204351ca
melapeyre <mileslapeyre@gmail.com>
2022-07-12 10:55:43 -0500

    Dynamically display current inventory

--- src/pages/CurrentInventory.js
+++ src/pages/CurrentInventory.js
@@ -1,32 +1,62 @@
-import React from 'react';
+import React from "react";
+
+//mapping name and count from full inventory list
+function mapperFunc (obj) {
+    return <p>{obj.name}({obj.sku}):{obj.count}</p>
+}
 
 class CurrentInventory extends React.Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            currentInventoryLevels: []
-        }
-    }
+  constructor(props) {
+	super(props);
 
-    async componentDidMount(){
-        var requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-        };
-        
-        fetch("https://2nto10r5ua.execute-api.us-east-1.amazonaws.com/getCurrentFinishedInventory", requestOptions)
-            .then(response => response.json())
-            .then((data) => {
-                this.setState({
-                    currentInventoryLevels: data
-                })
-            })
-    }
+	this.state = {
+	  currentInventoryLevels: [],
+	};
+  }
+//requesting data from lambda
+  async componentDidMount() {
+	var requestOptions = {
+	  method: "GET",
+	  redirect: "follow",
+	};
 
-    render() {
-        return <p>{JSON.stringify(this.state.currentInventoryLevels)}</p>
-    }
+	let response = await fetch(
+	  "https://2nto10r5ua.execute-api.us-east-1.amazonaws.com/getCurrentFinishedInventory",
+	  requestOptions
+	);
+	let data = await response.json();
+	this.setState({
+		currentInventoryLevels: data,
+	});
+  }
+//displaying current inventory grouped by product type
+render() {
+	//if no data loads - return loading 
+	  if (this.state.currentInventoryLevels.length === 0) return <p>Loading...</p>;
+
+	//filter data by product type
+	let boilBossElements = this.state.currentInventoryLevels.filter(
+	  (x) => x.productType === "BoilBoss"
+	);
+	let paddleElements = this.state.currentInventoryLevels.filter(
+	  (x) => x.productType === "Paddle"
+	);
+	
+	//display data for both product types
+	return (
+	  <div>
+		<div>
+		  <h1>BoilBoss</h1>
+		  {boilBossElements.map(mapperFunc)}
+		</div>
+
+		<div>
+		  <h1>Thermo-Paddle</h1>
+		  {paddleElements.map(mapperFunc)}
+		</div>
+	  </div>
+	);
+  }
 }
 
 export default CurrentInventory;
