853c427e39811f1fbec407e84124e6ee24d8224a
melapeyre <mileslapeyre@gmail.com>
2022-07-17 12:17:36 -0500

    production/inventory page

---
+++ src/Components/InventoryInput.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export class InventoryInput extends React.Component {
+    constructor (props){
+        super(props);
+
+        this.state = {
+
+        };
+    }
--- src/pages/Production.js
+++ src/pages/Production.js
@@ -1,5 +1,44 @@
+import { button } from 'aws-amplify';
 import React from 'react';
 
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify([
+  {
+    "sku": "sp",
+    "qty": 10,
+    "producer": "Alex"
+  }
+]);
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+let response = await fetch(
+	"https://2nto10r5ua.execute-api.us-east-1.amazonaws.com/default/InventoryProduced", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
 export default function Production() {
-	return <p>This is the production page</p>
+	return(
+<div>
+	<div> 
+	<h1>BoilBoss Production</h1>
+	<button>Standard Black</button>
+	<button>Standard Red</button>
+	<button>Standard Purple and Gold</button>
+	</div>
+	
+	<div>
+		<h1>Paddle Production</h1>
+	</div>
+	<button>Thermo Paddle</button>
+ </div>
+	)
 }
